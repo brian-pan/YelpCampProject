2508e5781f842cc5696ce5b7a90978db21f850b5
Brian Pan <brianpanzifan@gmail.com>
2022-03-06 00:22:04 -0500

    C.R.U doen

--- app.js
+++ app.js
@@ -13,19 +13,51 @@ db.once('open', () => {
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const methodOverride = require('method-override');
 const Campground = require('./models/campground')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true})) //parse req.body
+app.use(methodOverride('_method'))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: 'My backyard', description: 'Cheap Camping'})
-    await camp.save();
-    res.send(camp)
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+app.post('/campgrounds', async (req,res) => {
+    //under campground
+    //res.send(req.body.campground)
+    //make a new campground:
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    // const id = req.params.id;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground })
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground })
+})
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new: true});
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.listen(3000, () => {
